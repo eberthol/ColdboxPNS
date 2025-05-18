809b4b7a0e375fa9d7a4fa1cddc664235302d739
Emilie Bertholet <emiliebertholet@Emilie.local>
2025-05-18 16:12:45 +0300

    add peak heigts in peak extraction

--- peak_extraction.py
+++ peak_extraction.py
@@ -13,14 +13,18 @@ def find_peaks_2d(array_2d, **kwargs):
     masks = []
     props_list = []
     peak_indices = []
+    peak_heights = []
     flat_peak_indices = []
 
     for row_idx, row in enumerate(array_2d):
         peaks, props = find_peaks(row, **kwargs)
+        heights = props.get("peak_heights", row[peaks])
         mask = np.isin(np.arange(row.size), peaks)
+
         masks.append(mask)
         props_list.append(props)
         peak_indices.append(peaks)
+        peak_heights.append(heights)
         flat_peak_indices.extend([(row_idx, col) for col in peaks])
 
     peak_masks = np.array(masks)
@@ -28,6 +32,7 @@ def find_peaks_2d(array_2d, **kwargs):
     return {
         "mask": peak_masks,
         "row_peak_indices": peak_indices,
+        "peak_heights": peak_heights,
         "properties": props_list,
         "flat_peak_indices": flat_peak_indices
     }
@@ -49,56 +54,56 @@ if __name__=="__main__":
     folder_base = '/Users/emiliebertholet/cernbox/coldbox_data'
 
     
-    # save_file(intput_file=f'{folder_base}/adc_data_run_25036_ch_40_all.npy', # neutrons
-    #           output_file='data_tests/peaks_neutrons_wlenNone.pkl', 
-    #           wlen=None)
-
-    # save_file(intput_file=f'{folder_base}/adc_data_run_25036_ch_40_all.npy', # neutron selection
-    #         output_file='data_tests/peaks_neutronsSel_wlenNone.pkl', 
-    #         wlen=None, cut=(3000,6000))
-    
-    # save_file(intput_file=f'{folder_base}/adc_data_run_25066_ch_40_all.npy', # cosmics
-    #           output_file='data_tests/peaks_cosmics_wlenNone.pkl', 
-    #           wlen=None)
-
-    # save_file(intput_file=f'{folder_base}/adc_data_run_25036_ch_40_all.npy', # neutrons
-    #           output_file='data_tests/peaks_neutrons_wlen500.pkl', 
-    #           wlen=500)
-
-    # save_file(intput_file=f'{folder_base}/adc_data_run_25036_ch_40_all.npy', # neutron selection
-    #         output_file='data_tests/peaks_neutronsSel_wlen500.pkl', 
-    #         wlen=None, cut=(3000,6000))
-    
-    # save_file(intput_file=f'{folder_base}/adc_data_run_25066_ch_40_all.npy', # cosmics
-    #           output_file='data_tests/peaks_cosmics_wlen500.pkl', 
-    #           wlen=500)
-
-
-
-
     save_file(intput_file=f'{folder_base}/adc_data_run_25036_ch_40_all.npy', # neutrons
-              output_file='data_tests/peaks_neutrons_wlenNone_prom500.pkl', 
-              wlen=None, prominence=500)
+              output_file='data_tests/peaks_neutrons_wlenNone.pkl', 
+              wlen=None)
 
     save_file(intput_file=f'{folder_base}/adc_data_run_25036_ch_40_all.npy', # neutron selection
-            output_file='data_tests/peaks_neutronsSel_wlenNone_prom500.pkl', 
-            wlen=None, cut=(3000,6000), prominence=500)
+            output_file='data_tests/peaks_neutronsSel_wlenNone.pkl', 
+            wlen=None, cut=(3000,6000))
     
     save_file(intput_file=f'{folder_base}/adc_data_run_25066_ch_40_all.npy', # cosmics
-              output_file='data_tests/peaks_cosmics_wlenNone_prom500.pkl', 
-              wlen=None, prominence=500)
+              output_file='data_tests/peaks_cosmics_wlenNone.pkl', 
+              wlen=None)
 
     save_file(intput_file=f'{folder_base}/adc_data_run_25036_ch_40_all.npy', # neutrons
-              output_file='data_tests/peaks_neutrons_wlen500_prom500.pkl', 
-              wlen=500, prominence=500)
+              output_file='data_tests/peaks_neutrons_wlen500.pkl', 
+              wlen=500)
 
     save_file(intput_file=f'{folder_base}/adc_data_run_25036_ch_40_all.npy', # neutron selection
-            output_file='data_tests/peaks_neutronsSel_wlen500_prom500.pkl', 
-            wlen=None, cut=(3000,6000), prominence=500)
+            output_file='data_tests/peaks_neutronsSel_wlen500.pkl', 
+            wlen=None, cut=(3000,6000))
     
     save_file(intput_file=f'{folder_base}/adc_data_run_25066_ch_40_all.npy', # cosmics
-              output_file='data_tests/peaks_cosmics_wlen500_prom500.pkl', 
-              wlen=500, prominence=500)
+              output_file='data_tests/peaks_cosmics_wlen500.pkl', 
+              wlen=500)
+
+
+
+
+    # save_file(intput_file=f'{folder_base}/adc_data_run_25036_ch_40_all.npy', # neutrons
+    #           output_file='data_tests/peaks_neutrons_wlenNone_prom500.pkl', 
+    #           wlen=None, prominence=500)
+
+    # save_file(intput_file=f'{folder_base}/adc_data_run_25036_ch_40_all.npy', # neutron selection
+    #         output_file='data_tests/peaks_neutronsSel_wlenNone_prom500.pkl', 
+    #         wlen=None, cut=(3000,6000), prominence=500)
+    
+    # save_file(intput_file=f'{folder_base}/adc_data_run_25066_ch_40_all.npy', # cosmics
+    #           output_file='data_tests/peaks_cosmics_wlenNone_prom500.pkl', 
+    #           wlen=None, prominence=500)
+
+    # save_file(intput_file=f'{folder_base}/adc_data_run_25036_ch_40_all.npy', # neutrons
+    #           output_file='data_tests/peaks_neutrons_wlen500_prom500.pkl', 
+    #           wlen=500, prominence=500)
+
+    # save_file(intput_file=f'{folder_base}/adc_data_run_25036_ch_40_all.npy', # neutron selection
+    #         output_file='data_tests/peaks_neutronsSel_wlen500_prom500.pkl', 
+    #         wlen=None, cut=(3000,6000), prominence=500)
+    
+    # save_file(intput_file=f'{folder_base}/adc_data_run_25066_ch_40_all.npy', # cosmics
+    #           output_file='data_tests/peaks_cosmics_wlen500_prom500.pkl', 
+    #           wlen=500, prominence=500)
 
 
  
