4c48e4dec2cc505867680e059d354ea0be0eae89
Emilie Bertholet <emiliebertholet@Emilie.local>
2025-08-13 10:05:29 +0300

    updates

--- process_files.py
+++ process_files.py
@@ -8,6 +8,8 @@ import uproot
 import awkward as ak
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm
+import h5py
+import glob
 
 import time
 
@@ -17,12 +19,8 @@ def read_root_files(filepath, cosmics=False):
     analyszer = 'PNSCRPanalyzercosmics' if cosmics else 'PNSCRPanalyzerflashes1'
     evts = uproot.open(f"{filepath}:{analyszer}/Event")
     akw = evts.arrays(filter_name=evts.keys(), library="ak")
-    print('number of trigger records:', len(akw['event']))
+    # print('number of trigger records:', len(akw['event']))
     return akw
-
-def save_to_pkl(akw, output_filename):
-    with open(output_filename, "wb") as f:
-        pickle.dump(akw, f)
 
 def get_info(akw, display=False):
     # akw['waveformsADC'][trigRec][chID]
@@ -39,18 +37,17 @@ def get_info(akw, display=False):
         print(' max height', int(np.max(maxima)) )
     return Nevents, TrigWindow, Nchannels, maxima
 
-def subtract_pedestals_all_channels_np(akw, nmax=None):
+def subtract_pedestals_all_channels_np(akw, nmax=None, to_h5=False, filename='data'):
     waveforms = akw['waveformsADC']
-    nevents = len(waveforms) if nmax is None else min(nmax, len(waveforms))
+    nevents   = len(waveforms) if nmax is None else min(nmax, len(waveforms))
     waveforms = waveforms[:nevents]
 
     # Determine number of channels and maximum waveform length
     n_channels = max(len(evt) for evt in waveforms)
-    max_len = max(len(ch) for evt in waveforms for ch in evt)
+    max_len    = max(len(ch) for evt in waveforms for ch in evt)
 
     # Preallocate output arrays
-    waveformsADC_np = np.full((nevents, n_channels, max_len), np.nan, dtype=np.float32)
-    waveformsSubtracted_np = np.full((nevents, n_channels, max_len), np.nan, dtype=np.float32)
+    waveformsADC_np = np.full((nevents, n_channels, max_len), np.nan, dtype=np.int32)
     pedestals_np = np.zeros((nevents, n_channels), dtype=np.int32)
 
     for i_evt, evt in enumerate(tqdm(waveforms, desc="Subtracting pedestals")):
@@ -61,35 +58,87 @@ def subtract_pedestals_all_channels_np(akw, nmax=None):
             pedestals_np[i_evt, i_ch] = pedestal
             sub = ch_np - pedestal
             waveformsADC_np[i_evt, i_ch, :len(ch_np)] = ch_np
-            waveformsSubtracted_np[i_evt, i_ch, :len(sub)] = sub
 
-    # Wrap into awkward array with fields
-    result = ak.Array({
-        'waveformsADC': waveformsADC_np,
-        'waveformsSubtracted': waveformsSubtracted_np,
-        'pedestals': pedestals_np,
-    })
-    return result
+    if to_h5:
+        with h5py.File(filename, "w") as f:
+            f.create_dataset("waveformsADC", data=waveformsADC_np, compression="gzip")
+            f.create_dataset("pedestals", data=pedestals_np)
+    else:
+        with open(filename, "wb") as f:
+            pickle.dump({
+                "waveformsADC": waveformsADC_np,
+                "pedestals": pedestals_np,
+            }, f)
 
+def main(sample, runNumber, savetoh5, input_files):
+    for filepath in input_files:
+        print('process file', filepath)
+        info=filepath.split('/')[-1].split('.')[0].split('_')
+        id_first, id_last = info[-2], info[-1]
+        outputName = f'{sample}_nTuples_r{runNumber}_{id_first}-{id_last}'
+        if sample=='pns':
+            akw = read_root_files(filepath, cosmics=False)
+            print('\nPNS')
+        else:
+            akw = read_root_files(filepath, cosmics=True)
+            print('\nCosmics')
+        get_info(akw, display=True)
+        if savetoh5:
+            subtract_pedestals_all_channels_np(akw, nmax=None, to_h5=True, filename=f'{filepath}/{outputName}.h5')
+        else:
+            subtract_pedestals_all_channels_np(akw, nmax=None, to_h5=False, filename=f'{filepath}/{outputName}.pkl')
+
+def read_dataset(file, key):
+    with h5py.File(file, "r") as h5:
+        return h5[key][:]
+    
 if __name__=="__main__":
     cernbox = '/Users/emiliebertholet/cernbox/coldbox_data'
+    filepath = f'{cernbox}/anaCRP_files'
 
-    ## PNS
-    filepath = f'{cernbox}/anaCRP_files/ana_pns_small.root' 
-    akw_pns = read_root_files(filepath, cosmics=False)
-    print('\nPNS')
-    get_info(akw_pns, display=True)
-    result = subtract_pedestals_all_channels_np(akw_pns, nmax=None)
-    save_to_pkl(result, 'akw_pns_pedSub.pkl')
+    ####### file by file
+    sample = 'cos'
 
-    ## Cosmics
-    filepath = f'{cernbox}/anaCRP_files/ana_cosmic_small.root' 
-    akw_cos = read_root_files(filepath, cosmics=True)
-    print('\nCosmics')
-    get_info(akw_cos, display=True)
-    result = subtract_pedestals_all_channels_np(akw_cos, nmax=None)
-    save_to_pkl(result, 'akw_cos_pedSub.pkl')
+    # if sample=='pns':
+    #     runNumber = 25036
+    #     # savetoh5 = True
+    #     input_files = [
+    #         f'{filepath}/ana_pns_r{runNumber}_small_0_7.root', 
+    #         f'{filepath}/ana_pns_r{runNumber}_small_8_16.root', 
+    #         f'{filepath}/ana_pns_r{runNumber}_small_17_30.root'
+    #     ]
+    # elif sample=='cos':
+    #     runNumber = 25004
+    #     # savetoh5 = True
+    #     input_files = [
+    #         f'{filepath}/ana_cosmic_r{runNumber}_small_0_6.root', 
+    #         f'{filepath}/ana_cosmic_r{runNumber}_small_7_15.root', 
+    #         f'{filepath}/ana_cosmic_r{runNumber}_small_16_30.root'
+    #     ]
+    # else:
+    #     sys.exit('error')
+
+    # main(sample, runNumber, savetoh5, input_files)
+
+    ####### merge files
+    sample = 'pns'
+    if sample=='pns':
+        runNumber = 25036
+        output_name = 'pns_nTuples_r{runNumber}_0-30.h5'
+    elif sample=='cos':
+        runNumber = 25004
+        output_name = 'cos_nTuples_r{runNumber}_0-30.h5'
 
 
+    files = sorted( glob.glob(os.path.join(filepath, f"{sample}_nTuples_r{runNumber}_*.h5")) )
+    pns = {
+        "waveformsADC": np.vstack([read_dataset(f, "waveformsADC") for f in files]),
+        "pedestals": np.concatenate([read_dataset(f, "pedestals") for f in files])
+    }
+    with h5py.File(f"{filepath}/{output_name}.h5", "w") as h5:
+        for key, arr in pns.items():
+            h5.create_dataset(key, data=arr)
+    
+    
 
 
